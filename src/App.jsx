1f7a011876dedd5ef8e5e60f22c859d7fbd85646
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-08-16 14:50:00 +0100

    code splitting set up and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react"
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import loadable from "@loadable/component"
 
-import Home from "./pages/Home"
-import Artefact from "./pages/Artefact"
+const Home = loadable(()=>import("./pages/Home"))
+const Artefact = loadable(()=>import("./pages/Artefact"))
 
 const App = () =>
   <Router>
+    <Link to="/">Home</Link>
+    <Link to="/artefact/derp">Artefact</Link>
     <Route path="/" component={Home} exact/>
     <Route path="/artefact/:id" component={Artefact} exact/>
   </Router>
-
 
 export default App
