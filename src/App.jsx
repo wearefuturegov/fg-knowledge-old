b51bce504f80d86e3bbc3a7238a616b4d98afa37
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-08-16 20:31:39 +0100

    add a 404 fallback route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react"
-import {BrowserRouter as Router, Link} from "react-router-dom"
+import {BrowserRouter as Router, Link, Switch} from "react-router-dom"
 import AccessibleRoute from "./components/AccessibleRoute"
 import loadable from "@loadable/component"
 
 const Home = loadable(()=>import("./pages/Home"))
 const Artefact = loadable(()=>import("./pages/Artefact"))
+const NotFound = loadable(()=>import("./pages/NotFound"))
 
 const App = () =>
   <Router>
     <Link to="/">Home</Link>
     <Link to="/artefact/derp">Artefact</Link>
 
-    <AccessibleRoute path="/" component={Home} exact/>
-    <AccessibleRoute path="/artefact/:id" component={Artefact} exact/>
+    <Switch>
+      <AccessibleRoute path="/" component={Home} exact />
+      <AccessibleRoute path="/artefact/:id" component={Artefact}/>
+      <AccessibleRoute component={NotFound}/>
+    </Switch>
+
   </Router>
 
 export default App
