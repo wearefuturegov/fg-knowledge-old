e6bc6ce22a4af78f854d169610c6801b3219d1aa
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-08-16 14:06:18 +0100

    created some scaffolding components and added typescript link

---
+++ src/components/SiteHeader/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import logo from "./logo.svg"
+
+const SiteHeader = () =>
+    <header>
+        <div>
+            <img src={logo} alt="FutureGov Knowledge"/>
+        </div>
+    </header>
+
+export default SiteHeader
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react"
+import SiteHeader from "../components/SiteHeader"
 
 const Home = () =>
-    <h1>Home page</h1>
+    <>
+        <h1>Home page</h1>
+        <SiteHeader/>
+    </>
 
 export default Home
